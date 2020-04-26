66a737d813dca5434a8afa5685be0efea36d8bea
mitucu <64152320+mitucu@users.noreply.github.com>
2020-04-26 11:02:30 +0300

    Add files via upload

---
+++ project1/build/CMakeFiles/p1hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p1hello.dir/script/p1hello.cpp.o"
+  "libp1hello.pdb"
+  "libp1hello.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p1hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
